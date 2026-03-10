3401d64c378cad7acc63fc54136895786b6108fd
TapasPulis <rooneysamragya10@gmail.com>
2026-03-10 22:48:41 +0100

    Added contact page

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { Route, Routes } from "react-router-dom";
 import MainLayout from "./layouts/main.layout";
 import HomePage from "./pages/home.page";
 import AboutPage from "./pages/about.page";
+import ContactPage from "./pages/contact.page";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         <Route element={<MainLayout />}>
           <Route path="/" element={<HomePage />} />
           <Route path="/about" element={<AboutPage />} />
+          <Route path="/contact" element={<ContactPage />} />
         </Route>
       </Routes>
     </>
---
+++ src/pages/contact.page.tsx
@@ -0,0 +1,21 @@
+const ContactPage = () => {
+  return (
+    <div className="contactFormContainer">
+      <form>
+        <h1>Contact Us</h1>
+        <label htmlFor="name">Name:</label>
+        <input type="text" id="name" name="name" required />
+
+        <label htmlFor="email">Email:</label>
+        <input type="email" id="email" name="email" required />
+
+        <label htmlFor="message">Message:</label>
+        <textarea id="message" name="message" required />
+
+        <button type="submit">Send Message</button>
+      </form>
+    </div>
+  );
+};
+
+export default ContactPage;
