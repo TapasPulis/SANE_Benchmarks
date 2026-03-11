432bbff4ba01d70a64a37f9aa3d0671bea6a3337
TapasPulis <rooneysamragya10@gmail.com>
2026-03-11 11:53:02 +0100

    Added placeholder to login form

--- src/pages/login.page.tsx
+++ src/pages/login.page.tsx
@@ -4,10 +4,22 @@ const LoginPage = () => {
       <h2>Log In</h2>
       <form>
         <label htmlFor="email">Email:</label>
-        <input type="email" id="email" name="email" required />
+        <input
+          type="email"
+          id="email"
+          name="email"
+          placeholder="Type your email"
+          required
+        />
 
         <label htmlFor="password">Password:</label>
-        <input type="password" id="password" name="password" required />
+        <input
+          type="password"
+          id="password"
+          name="password"
+          placeholder="Type your password"
+          required
+        />
 
         <button type="submit">Login</button>
       </form>
