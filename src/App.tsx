26dad98e0d4e3ffc706da137184cf8bd80a55387
TapasPulis <rooneysamragya10@gmail.com>
2026-03-11 16:51:03 +0100

    Added a service page with the plans

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import HomePage from "./pages/home.page";
 import AboutPage from "./pages/about.page";
 import ContactPage from "./pages/contact.page";
 import LoginPage from "./pages/login.page";
+import ServicePage from "./pages/service.page";
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
           <Route path="/about" element={<AboutPage />} />
           <Route path="/contact" element={<ContactPage />} />
           <Route path="/login" element={<LoginPage />} />
+          <Route path="/services" element={<ServicePage />} />
         </Route>
       </Routes>
     </>
---
+++ src/pages/service.page.tsx
@@ -0,0 +1,55 @@
+const ServicePage = () => {
+  return (
+    <div>
+      <div className="freePlan">
+        <h1>Free Plan</h1>
+        <p>Enjoy basic features for free!</p>
+        <ul>
+          <li>Access the company's benchmark tests</li>
+          <li>
+            Choose which settings you want to see from the company's tests
+          </li>
+          <li>Daily updates</li>
+          <li>Users see advertisements</li>
+        </ul>
+      </div>
+      <hr />
+      <div className="premiumPlan">
+        <h1>Premium Plan</h1>
+        <p>Unlock all features with our premium subscription!</p>
+        <ul>
+          <li>Personalized benchmark analysis</li>
+          <li>Automatically generated game settings for the user's device</li>
+          <li>
+            Estimated FPS for different games based on our benchmark tests
+          </li>
+          <li>The user can compare their PC with the company's test PC</li>
+          <li>Access to all benchmark tests for every platform</li>
+          <li>Ad-free experience</li>
+        </ul>
+      </div>
+      <div className="subscriptionsContainer">
+        <div className="subscriptionCard">
+          <h2>1 month</h2>
+          <p>40 SEK/month</p>
+
+          <button>Subscribe</button>
+        </div>
+        <div className="subscriptionCard">
+          <h2>3 months</h2>
+          <p>75 SEK = 25 SEK/month</p>
+
+          <button>Subscribe</button>
+        </div>
+        <div className="subscriptionCard">
+          <h2>12 months</h2>
+          <p>240 SEK = 20 SEK/month</p>
+
+          <button>Subscribe</button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ServicePage;
